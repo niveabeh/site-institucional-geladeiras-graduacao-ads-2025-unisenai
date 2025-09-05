516c17859aa0ebf88a4f6d65312dfe9dfa2c01af
niveabeh <niveasouza86@gmail.com>
2025-09-05 20:43:11 -0300

    add

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const formulario = document.getElementById("frmFormulario");
 let respAlert = document.getElementById("respAlert");
 
 formulario.addEventListener("submit", function (e) {
+
     e.preventDefault();
-    alert("entrou");
 
     const validaNome = /^[A-zà-ü]{3,}([ ][A-zà-ü]{2,}?)+$/ig;
     const validaFone = /^[(][0-9]{2}[)][ ][0-9]{4,5}[-][0-9]{4}$/;
@@ -14,29 +14,23 @@ formulario.addEventListener("submit", function (e) {
 
 
     if (!validaNome.test(nome)) {
-        alert("Por favor, preencha o campo com nome e sobrenome");
         respAlert.innerText = `Por favor, preencha o campo com nome e sobrenome`;
         nome.focus();
-        return false;
     }
 
     if (!validaFone.test(fone)) {
-        alert("Por favor, preencha o campo com DDD e número de telefone (ex: (11)99999-9999)");
         respAlert.innerText = `Por favor, preencha o campo com DDD e número de telefone (ex: (11)99999-9999)`;
         fone.focus();
-        return false;
+        
     }
 
     if (!validaEmail.test(email)) {
-        alert("Por favor, preencha o campo com um e-mail válido");
         respAlert.innerText = `Por favor, preencha o campo com um e-mail válido`;
         email.focus();
-        return false;
     }
 
-    respAlert.innerText = `Formulário enviado com sucesso!`;
-    alert("entrou");
-    return false;
+    respAlert.respAlert.innerText = `Formulário enviado com sucesso!`;
+
 });
 
 
